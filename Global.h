ff4ef2b6e9558997c9885d93d34d30b201689696
gigajet <gigajet08092000@gmail.com>
2019-07-19 06:49:06 +0700

    Sửa definition lần 1: inTitle
    19-07 6-48

---
+++ Global.h
@@ -0,0 +1,40 @@
+#pragma once
+#ifndef GLOBAL_H_
+#define GLOBAL_H_
+
+#include <iostream>
+#include <filesystem>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <set>
+#include <fstream>
+#include <sstream>
+#include "Trie.h"
+
+using namespace std;
+
+class Global
+{
+private:
+	static Global* instance;
+	Global();
+public:
+	Trie trie;
+	vector<string> fileName;
+	void ReadData(string path);
+	static Global* GetInstance();
+
+};
+
+set<int> And(set<int> prevRes, string keyword, bool inTitle); //1
+set<int> Or(set<int> prevRes, string keyword, bool inTitle); //2
+set<int> Exclude(set<int> prevRes, string keyword, bool inTitle); //3
+
+set<int> HashTag(set<int> prevRes, string keyword); //8
+set<int> ExactMatch(set<int> prevRes, string keyword, bool inTitle); //9
+
+
+#endif // !GLOBAL_H_
